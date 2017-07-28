cb863b6c1d52425e23fbaccbb98c35b9670f2f32
Gideon Hadikusumo <gideon.hadikusumo@gmail.com>
2017-07-28 17:21:05 +1000

    Fix missing imports

---
+++ example.py
@@ -0,0 +1,7 @@
+from led_matrix_scroller import scroll
+
+def main():
+    scroll('hello! ', 100)
+
+if __name__ == '__main__':
+    main()
--- led_matrix_scroller.py
+++ led_matrix_scroller.py
@@ -1,5 +1,6 @@
-import wemos_led_matrix
+from wemos_led_matrix import leds
 from time import sleep_ms
+from font8x8_basic import font8x8_basic 
 
 def shift_row(a, b, offset):
     return (a >> offset) | (b << (8 - offset))
