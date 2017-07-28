5e34865a7a842606759181b774effc9e9bd9aba8
Gideon Hadikusumo <gideon.hadikusumo@gmail.com>
2017-07-29 08:18:09 +1000

    Reversed the row order of the font

--- led_matrix_scroller.py
+++ led_matrix_scroller.py
@@ -1,12 +1,12 @@
 from wemos_led_matrix import leds
+from font8x8_basic import font
 from time import sleep_ms
-from font8x8_basic import font8x8_basic 
 
 def shift_row(a, b, offset):
     return (a >> offset) | (b << (8 - offset))
    
 def scroll(message, sleep_period):
-    for l1, l2 in zip(message[:-1], message[1:]):
-        for column in range(0, 8):
-            leds.send_bytes([int(shift_row(a, b, column)) for a,b in zip(list(font8x8_basic[l1])[::-1], list(font8x8_basic[l2])[::-1])])
+    for c1, c2 in zip(message[:-1], message[1:]):
+        for column in range(8):
+            leds.send_bytes(shift_row(a, b, column) for a, b in zip(font[c1], font[c2]))
             sleep_ms(sleep_period)
